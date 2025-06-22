fa77fe2c840906caa93e361c1cef5659ef2fd656
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-06-22 18:39:30 -0300

    Create infopessoa.java

---
+++ bloco-2-variaveis-operadores/infopessoa.java
@@ -0,0 +1,13 @@
+public class InfoPessoa{
+    public static void main(String[] args){
+        String nome = "Carlos";
+        int idade = 20;
+        double altura = 1.80;
+        boolean estudante = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura + "m");
+        System.out.println("É estudante? " + estudante);
+    }
+}
