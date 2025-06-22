d0281c54bf086e4109c371ecf0fc0fa46de686ef
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-06-22 18:43:30 -0300

    Create Tabuada.java

---
+++ bloco-4-repeticao/Tabuada.java
@@ -0,0 +1,9 @@
+public class Tabuada{
+    public static void main(String[] args){
+        int multiplier = 5;
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(multiplier * i);
+        }
+    }
+}
