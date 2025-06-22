f581d274c2790c270e982f86a22db5dd8399230f
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-06-22 18:40:27 -0300

    Create ClassificacaoAluno.java

---
+++ bloco-3-condicionais/ClassificacaoAluno.java
@@ -0,0 +1,17 @@
+public class ClassificacaoAluno{
+    public static void main(String[] args) {
+        int note = 60;
+
+        if (note >= 100) {
+            System.out.println("Perfect!");
+        } else if (note >= 90) {
+            System.out.println("Very good!");
+        } else if (note >= 70) {
+            System.out.println("Good");
+        } else if (note >= 60) {
+            System.out.println("Approved");
+        } else {
+            System.out.println("Failed. Study more");
+        }
+    }
+}
