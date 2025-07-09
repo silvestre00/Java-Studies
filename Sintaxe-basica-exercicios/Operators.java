0651e208af879004819b02e1f3675c83c4794faa
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-07-09 20:15:01 -0300

    Create Operators.java

---
+++ Sintaxe-basica-exercicios/Operators.java
@@ -0,0 +1,20 @@
+public class Operators {
+    public static void main(String[] args){
+        int num1 = 11;
+        int num2 = 7;
+
+        System.out.println("Logical Operators: ");
+        System.out.println("The sum of number 1 and number 2 is: " + (num1 + num2));
+        System.out.println("The subtraction between the number 1 and the number 2 is: " + (num1 - num2));
+        System.out.println("The multiplication between the number 1 and the number 2 is: " + (num1 * num2));
+        System.out.println("The division between the number 1 and the number 2 is: " + (num1 / num2));
+        System.out.println("The remainder of division between the number 1 and the number 2 is: " + (num1 % num2));
+        System.out.println("Relational Operators: ");
+        System.out.println("Is the number 1 the same as the number 2? " + (num1 == num2));
+        System.out.println("Is number 1 different from number 2? " + (num1 != num2));
+        System.out.println("Is number 1 greater than number 2? " + (num1 > num2));
+        System.out.println("Is the number 1 greater than or equal to the number 2? " + (num1 >= num2));
+        System.out.println("Is number 1 smaller than number 2? " + (num1 < num2));
+        System.out.println("Is the number 1 less than or equal to the number 2? " + (num1 <= num2));
+    }
+}
