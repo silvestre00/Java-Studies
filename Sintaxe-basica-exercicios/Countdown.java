24f2aee1a2ac0b9ceae3114d1759c5269b682c98
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-07-09 20:14:38 -0300

    Create Countdown.java

---
+++ Sintaxe-basica-exercicios/Countdown.java
@@ -0,0 +1,11 @@
+public class Countdown {
+    public static void main(String[] args){
+        int i = 10;
+        System.out.println("Starting countdown...");
+        while(i >= 0){
+            System.out.println(i);
+            i--;
+        }
+        System.out.println("Launch!!!");
+    }
+}
