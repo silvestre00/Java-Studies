341cd917e3b505fc05f375a235f52503df8b6364
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-07-23 20:26:11 -0300

    Update Main.java
    Adjustment in main to display results with all products that I have already created classes for.

--- bloco7-POO/Package-Desafio/Main.java
+++ bloco7-POO/Package-Desafio/Main.java
@@ -2,15 +2,22 @@ package Desafio;
 
 public class Main {
     public static void main(String[] args){
-        // Criando objeto e definindo valores
-        Product product1 = new Product("Ketchup", 12, 0);
-        // Chamadas
-        product1.displayDetails();
-        product1.setName("Ice Cream");
-        System.out.println("New product name is: " + product1.getName());
-        product1.setPrice(-10);
-        product1.setQuantityInStock(2);
-        System.out.println("Quantity in stock is: " + product1.getQuantityInStock());
-        product1.addStock(3);
+        Product productGeneric = new Product("Pen", 2.0, 100);
+
+        System.out.println("---Generic Product Details---");
+        productGeneric.displayDetails();
+
+        System.out.println("\n--- Book details ---");
+        Book book1 = new Book("O Senhor dos Anéis", 80.0, 10, "J.R.R. Tolkien", 1200);
+        book1.displayDetails();
+        book1.sell(3);
+
+        System.out.println("\n--- Electronic Details ---");
+        Eletronics eletronic1 = new Eletronics("Computer", 2500, 10, "Positivo", 220);
+        eletronic1.displayDetails();
+
+        System.out.println("\n--- Sauces Details ---");
+        Sauces sauce1 = new Sauces("Ketchup", 1.50, 100, "Picante", 50);
+        sauce1.displayDetails();
     }
 }
