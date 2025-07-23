37a86062eaf7a099bce1949d54f01658f359356f
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-07-23 20:36:35 -0300

    Create Main.java
    Poo practice on banks.

---
+++ bloco-7-POO/Package-Bank/Main.java
@@ -0,0 +1,16 @@
+public class Bank {
+    public static void main(String[] args){
+        AccountBanking account1 = new AccountBanking("0001-01", "Silvestre");
+        account1.deposit(100.00);
+        account1.checkBalance();
+        account1.withdrawMoney(200);
+        account1.withdrawMoney(50);
+        System.out.println("--------------");
+        AccountBanking account2 = new AccountBanking("0001-02", "Ana");
+        account2.deposit(1000.00);
+        account2.checkBalance();
+        account2.withdrawMoney(900);
+        account2.withdrawMoney(200);
+        account2.checkBalance();
+    }
+}
