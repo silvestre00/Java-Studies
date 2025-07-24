dd7a621e5f64e479187271fff6d9ec50f7ac65e6
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-07-24 14:33:14 -0300

    Update Main.java
    feat(Main): Adds tests for new package components

--- bloco-7-POO/Package-Bank/Main.java
+++ bloco-7-POO/Package-Bank/Main.java
@@ -12,5 +12,24 @@ public class Main {
         account2.withdrawMoney(900);
         account2.withdrawMoney(200);
         account2.checkBalance();
+        System.out.println("------Test gets-------");
+        //Builder account 3
+        AccountBanking account3 = new AccountBanking("0009-01", "Input Test");
+        //Get Account Banking
+        String accNumber3 = account3.getAccountNumber();
+        System.out.println("Account number: " + accNumber3);
+        //Get Balance
+        double balanceAcc3 = account3.getBalance();
+        System.out.println("Balance get test, starting balance: R$" + balanceAcc3);
+        account3.deposit(250);
+        balanceAcc3 = account3.getBalance();
+        System.out.println("Balance get test, balance after deposit: R$" + balanceAcc3);
+        //Get Cardholder Name
+        String nameAcc3 = account3.getCardholderName();
+        System.out.println("The account holder's name before the change is: " + nameAcc3);
+        System.out.println("------Test Set-------");
+        account3.setCardholderName(" ");
+        nameAcc3 = account3.getCardholderName();
+        System.out.println("The account holder name after the change is: " + nameAcc3);
     }
 }
