58cce13f5e21df2650c6c263847ba44f9fcdd4f9
silvestre00 <136522337+silvestre00@users.noreply.github.com>
2025-07-26 13:32:03 -0300

    Update Main.java
    test(main): Update main method to test CurrentAccount
    
    The previous test code in the main method has been commented out to focus on testing the new account types.
    
    This commit adds a new set of test cases specifically for the `CurrentAccount` class, verifying its deposit and overdraft withdrawal functionality.

--- bloco-7-POO/Package-Bank/Main.java
+++ bloco-7-POO/Package-Bank/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args){
-        AccountBanking account1 = new AccountBanking("0001-01", "Silvestre");
+        /* Account account1 = new Account("0001-01", "Silvestre");
         account1.deposit(100.00);
         account1.checkBalance();
         account1.withdrawMoney(200);
         account1.withdrawMoney(50);
         System.out.println("--------------");
-        AccountBanking account2 = new AccountBanking("0001-02", "Ana");
+        Account account2 = new Account("0001-02", "Ana");
         account2.deposit(1000.00);
         account2.checkBalance();
         account2.withdrawMoney(900);
@@ -14,7 +14,7 @@ public class Main {
         account2.checkBalance();
         System.out.println("------Test gets-------");
         //Builder account 3
-        AccountBanking account3 = new AccountBanking("0009-01", "Input Test");
+        Account account3 = new Account("0009-01", "Input Test");
         //Get Account Banking
         String accNumber3 = account3.getAccountNumber();
         System.out.println("Account number: " + accNumber3);
@@ -31,5 +31,18 @@ public class Main {
         account3.setCardholderName(" ");
         nameAcc3 = account3.getCardholderName();
         System.out.println("The account holder name after the change is: " + nameAcc3);
+        System.out.println("------Savings Account-------");
+        SavingsAccount account4 = new SavingsAccount("0002-01", "Barsi", 12);
+        account4.deposit(1000);
+        account4.earnInterest();
+        account4.withdrawMoney(120);*/
+        System.out.println("------Current Account-------");
+        CurrentAccount accCurrent1 = new CurrentAccount("0001-01", "Silvestre", 1000);
+        accCurrent1.deposit(200);
+        accCurrent1.withdrawMoney(200);
+        accCurrent1.checkBalance();
+        accCurrent1.withdrawMoney(500);
+        accCurrent1.checkBalance();
+        accCurrent1.withdrawMoney(650);
     }
 }
